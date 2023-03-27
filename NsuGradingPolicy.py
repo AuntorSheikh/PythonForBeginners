4a3ee331caa0526855d43146f7a79569e6bcf007
Md_Auntor_Shak <53232134+AuntorSheikh@users.noreply.github.com>
2023-03-27 17:54:44 +0600

    Add files via upload

---
+++ NsuGradingPolicy.py
@@ -0,0 +1,25 @@
+marks= int(input("Enter Marks: "))
+
+if marks>=93:
+    print("A")
+elif marks >= 90:
+    print("A-")
+elif marks >= 87:
+    print("B+")
+elif marks >= 83:
+    print("B")
+elif marks >= 80:
+    print("B-")
+elif marks >= 77:
+    print("C+")
+elif marks >= 73:
+    print("C")
+elif marks >= 70:
+    print("C-")
+elif marks >= 67:
+    print("D+")
+elif marks >= 60:
+    print("D")
+else:
+    print("F")
+
